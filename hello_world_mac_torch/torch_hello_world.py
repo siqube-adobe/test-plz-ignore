998e86273488f2027cd772fee693b8b92ab88799
siqube-adobe <dreich@adobe.com>
2023-08-10 13:02:43 -0400

    test mps backend for torch

---
+++ hello_world_mac_torch/torch_hello_world.py
@@ -0,0 +1,121 @@
+# adjust from torch tutorials: https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
+from __future__ import annotations
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+
+def get_data() -> tuple[DataLoader, ...]:
+    training_data = datasets.FashionMNIST(
+        root='data',
+        train=True,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    test_data = datasets.FashionMNIST(
+        root='data',
+        train=False,
+        download=True,
+        transform=ToTensor(),
+    )
+
+    batch_size = 64
+
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+    for X, y in test_dataloader:
+        print(f'Shape of X [N, C, H, W]: {X.shape}')
+        print(f'Shape of y: {y.shape} {y.dtype}')
+        break
+    return train_dataloader, test_dataloader
+
+
+def get_device() -> str:
+    device = 'cuda' if torch.backends.mps.is_available() else 'cpu'
+    print(f'Using {device} device')
+    return device
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_gelu_stack = nn.Sequential(
+            nn.Linear(28*28, 512),
+            nn.GeLU(),
+            nn.Linear(512, 512),
+            nn.GeLU(),
+            nn.Linear(512, 10),
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_gelu_stack(x)
+        return logits
+
+
+def train(dataloader, model, loss_fn, optimizer, device):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f'loss: {loss:>7f}  [{current:>5d}/{size:>5d}]')
+
+
+def test(dataloader, model, loss_fn, device):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(
+        f'Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n',
+    )
+
+
+def main() -> int:
+    train_dataloader, test_dataloader = get_data()
+    device = get_device()
+    model = NeuralNetwork().to(device)
+    print(model)
+
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    epochs = 5
+    for t in range(epochs):
+        print(f'Epoch {t+1}\n-------------------------------')
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        test(test_dataloader, model, loss_fn, device)
+    print('Done!')
+
+    torch.save(model.state_dict(), 'model.pth')
+    print('Saved PyTorch Model State to model.pth')
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
